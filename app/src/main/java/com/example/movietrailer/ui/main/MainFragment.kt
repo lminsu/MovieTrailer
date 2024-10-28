39412d6fb4df9d81bbc0f99f7259a85974f1c28a
lminsu <lminsu@github.com>
2024-10-29 03:26:57 +0900

    lifecycle 콜백 추가

--- app/src/main/java/com/example/movietrailer/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/movietrailer/ui/main/MainFragment.kt
@@ -1,7 +1,9 @@
 package com.example.movietrailer.ui.main
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,88 +18,131 @@ import com.example.movietrailer.ui.pager.PagerFragment
 
 class MainFragment : Fragment() {
 
-    companion object {
-        private const val MOVIE_ID = "movie_id"
+	companion object {
+		private val TAG: String = MainFragment::class.java.simpleName
+		private const val MOVIE_ID = "movie_id"
 
-        fun newInstance(movieId: Long) = MainFragment().apply {
-            arguments = Bundle().apply {
-                putLong(MOVIE_ID, movieId)
-            }
-        }
-    }
+		fun newInstance(movieId: Long) = MainFragment().apply {
+			arguments = Bundle().apply {
+				putLong(MOVIE_ID, movieId)
+			}
+		}
+	}
 
-    private val viewModel: MainViewModel by lazy { makeMainViewModel() }
-    private var _binding: FragmentMainBinding? = null
-    private val binding: FragmentMainBinding get() = _binding!!
-    private var movieId: Long? = null
+	private val viewModel: MainViewModel by lazy { makeMainViewModel() }
+	private var _binding: FragmentMainBinding? = null
+	private val binding: FragmentMainBinding get() = _binding!!
+	private var movieId: Long? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            movieId = it.getLong(MOVIE_ID)
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		arguments?.let {
+			movieId = it.getLong(MOVIE_ID)
+		}
+	}
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		Log.d(TAG, "$TAG > ${hashCode()} > onCreateView")
+		_binding = FragmentMainBinding.inflate(inflater, container, false)
+		return binding.root
+	}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        initYouTubePlayerController()
-        initMainViewController()
-        movieId?.let {
-            val movie = getMovie(movieId = it) ?: return@let
-            viewModel.loadVideo(movie = movie)
-        }
-    }
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		Log.d(TAG, "$TAG > ${hashCode()} > onViewCreated")
+		initYouTubePlayerController()
+		initMainViewController()
+		movieId?.let {
+			val movie = getMovie(movieId = it) ?: return@let
+			viewModel.loadVideo(movie = movie)
+		}
+	}
 
-    private fun initYouTubePlayerController() {
-        with(binding.youtubePlayerView) {
-            YouTubePlayerController(
-                viewLifecycleOwner = viewLifecycleOwner,
-                viewModelStoreOwner = this@MainFragment,
-                playerView = this
-            )
-            lifecycle.addObserver(observer = this)
-        }
-    }
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		Log.d(TAG, "$TAG > ${hashCode()} > onAttach")
+	}
 
-    private fun initMainViewController() {
-        MainViewController(
-            binding = binding,
-            viewModelStoreOwner = this@MainFragment,
-            viewLifecycleOwner = viewLifecycleOwner
-        ).run {
-            initStatusBarHeight(height = getStatusBarHeight())
-        }
-    }
+	override fun onStart() {
+		super.onStart()
+		Log.d(TAG, "$TAG > ${hashCode()} > onStart")
+	}
+
+	override fun onResume() {
+		super.onResume()
+		Log.d(TAG, "$TAG > ${hashCode()} > onResume")
+	}
+
+	override fun onPause() {
+		super.onPause()
+		Log.d(TAG, "$TAG > ${hashCode()} > onPause")
+	}
+
+	override fun onStop() {
+		super.onStop()
+		Log.d(TAG, "$TAG > ${hashCode()} > onStop")
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+		Log.d(TAG, "$TAG > ${hashCode()} > onDestroyView")
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		Log.d(TAG, "$TAG > ${hashCode()} > onDestroy")
+	}
+
+	override fun onDetach() {
+		super.onDetach()
+		Log.d(TAG, "$TAG > ${hashCode()} > onDetach")
+	}
+
+	private fun initYouTubePlayerController() {
+		with(binding.youtubePlayerView) {
+			YouTubePlayerController(
+				viewLifecycleOwner = viewLifecycleOwner,
+				viewModelStoreOwner = this@MainFragment,
+				playerView = this
+			)
+			lifecycle.addObserver(observer = this)
+		}
+	}
+
+	private fun initMainViewController() {
+		MainViewController(
+			binding = binding,
+			viewModelStoreOwner = this@MainFragment,
+			viewLifecycleOwner = viewLifecycleOwner
+		).run {
+			initStatusBarHeight(height = getStatusBarHeight())
+		}
+	}
 
 
-    @SuppressLint("InternalInsetResource", "DiscouragedApi")
-    fun getStatusBarHeight(): Int {
-        var result = 0
-        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-        if (resourceId > 0) {
-            result = resources.getDimensionPixelSize(resourceId)
-        }
-        return result
-    }
+	@SuppressLint("InternalInsetResource", "DiscouragedApi")
+	fun getStatusBarHeight(): Int {
+		var result = 0
+		val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+		if (resourceId > 0) {
+			result = resources.getDimensionPixelSize(resourceId)
+		}
+		return result
+	}
 
-    private fun getMovie(movieId: Long): Movie? {
-        return (parentFragment as? PagerFragment)?.getMovie(movieId)
-    }
+	private fun getMovie(movieId: Long): Movie? {
+		return (parentFragment as? PagerFragment)?.getMovie(movieId)
+	}
 
-    // todo: hilt 사용
-    @Suppress("UNCHECKED_CAST")
-    private fun makeMainViewModel(): MainViewModel {
-        return ViewModelProvider(owner = this, object : ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return MainViewModel(repository = VideoRepository()) as T
-            }
-        })[MainViewModel::class.java]
-    }
+	// todo: hilt 사용
+	@Suppress("UNCHECKED_CAST")
+	private fun makeMainViewModel(): MainViewModel {
+		return ViewModelProvider(owner = this, object : ViewModelProvider.Factory {
+			override fun <T : ViewModel> create(modelClass: Class<T>): T {
+				return MainViewModel(repository = VideoRepository()) as T
+			}
+		})[MainViewModel::class.java]
+	}
 }
--- app/src/main/java/com/example/movietrailer/ui/pager/PagerFragment.kt
+++ app/src/main/java/com/example/movietrailer/ui/pager/PagerFragment.kt
@@ -1,6 +1,8 @@
 package com.example.movietrailer.ui.pager
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,52 +21,99 @@ import kotlinx.coroutines.launch
 
 class PagerFragment : Fragment() {
 
-    private val viewModel: PagerViewModel by lazy{ makePagerViewModel() }
-    private var _binding: FragmentPagerBinding? = null
-    private val binding: FragmentPagerBinding get() = _binding!!
+	companion object {
+		private val TAG: String = PagerFragment::class.java.simpleName
+	}
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentPagerBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+	private val viewModel: PagerViewModel by lazy { makePagerViewModel() }
+	private var _binding: FragmentPagerBinding? = null
+	private val binding: FragmentPagerBinding get() = _binding!!
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        collects()
-        viewModel.loadMovies()
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		Log.d(TAG, "$TAG > ${hashCode()} > onCreateView")
+		_binding = FragmentPagerBinding.inflate(inflater, container, false)
+		return binding.root
+	}
 
-    private fun initVideoPager(movieIds: List<Long>) {
-        binding.viewPageVideo.adapter = VideoPagerAdapter(
-            fragment = this,
-            movieIds = movieIds
-        )
-    }
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		Log.d(TAG, "$TAG > ${hashCode()} > onViewCreated")
+		collects()
+		viewModel.loadMovies()
+	}
 
-    private fun collects() {
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.movieIds.filterNotNull().collectLatest {
-                        initVideoPager(movieIds = it)
-                    }
-                }
-            }
-        }
-    }
+	override fun onAttach(context: Context) {
+		super.onAttach(context)
+		Log.d(TAG, "$TAG > ${hashCode()} > onAttach")
+	}
 
-    fun getMovie(movieId: Long): Movie? {
-        return viewModel.getMovie(movieId = movieId)
-    }
+	override fun onStart() {
+		super.onStart()
+		Log.d(TAG, "$TAG > ${hashCode()} > onStart")
+	}
 
-    private fun makePagerViewModel(): PagerViewModel {
-        return ViewModelProvider(owner = this, object : ViewModelProvider.Factory {
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                @Suppress("UNCHECKED_CAST")
-                return PagerViewModel(repository = MovieRepository()) as T
-            }
-        })[PagerViewModel::class.java]
-    }
+	override fun onResume() {
+		super.onResume()
+		Log.d(TAG, "$TAG > ${hashCode()} > onResume")
+	}
+
+	override fun onPause() {
+		super.onPause()
+		Log.d(TAG, "$TAG > ${hashCode()} > onPause")
+	}
+
+	override fun onStop() {
+		super.onStop()
+		Log.d(TAG, "$TAG > ${hashCode()} > onStop")
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+		Log.d(TAG, "$TAG > ${hashCode()} > onDestroyView")
+	}
+
+	override fun onDestroy() {
+		super.onDestroy()
+		Log.d(TAG, "$TAG > ${hashCode()} > onDestroy")
+	}
+
+	override fun onDetach() {
+		super.onDetach()
+		Log.d(TAG, "$TAG > ${hashCode()} > onDetach")
+	}
+
+	private fun initVideoPager(movieIds: List<Long>) {
+		binding.viewPageVideo.adapter = VideoPagerAdapter(
+			fragment = this,
+			movieIds = movieIds
+		)
+	}
+
+	private fun collects() {
+		lifecycleScope.launch {
+			viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+				launch {
+					viewModel.movieIds.filterNotNull().collectLatest {
+						initVideoPager(movieIds = it)
+					}
+				}
+			}
+		}
+	}
+
+	fun getMovie(movieId: Long): Movie? {
+		return viewModel.getMovie(movieId = movieId)
+	}
+
+	private fun makePagerViewModel(): PagerViewModel {
+		return ViewModelProvider(owner = this, object : ViewModelProvider.Factory {
+			override fun <T : ViewModel> create(modelClass: Class<T>): T {
+				@Suppress("UNCHECKED_CAST")
+				return PagerViewModel(repository = MovieRepository()) as T
+			}
+		})[PagerViewModel::class.java]
+	}
+
 }
